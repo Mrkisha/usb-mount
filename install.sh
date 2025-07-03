a45d3d340ce0ee44ae1b6d639d4556c5afd2af5f
Milos Zivanovic <miloszivanovic@hotmail.com>
2025-07-03 09:11:54 +0200

    fix

--- install.sh
+++ install.sh
@@ -42,8 +42,8 @@ if ! getent passwd 82 > /dev/null; then
   sudo useradd -u 82 www-data-nextcloud --system --no-create-home --shell /usr/sbin/nologin
 fi
 
-sudo usermod -aG $SHARED_GROUP 82
-echo -e "\033[0;33mAdded user '82' to group '$SHARED_GROUP'.\033[0m"
+sudo usermod -aG $SHARED_GROUP www-data-nextcloud
+echo -e "\033[0;33mAdded user 'www-data-nextcloud' to group '$SHARED_GROUP'.\033[0m"
 
 # Reload daemons
 echo "Reloading systemd and udev rules..."
