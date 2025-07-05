be4275d2f8e9f4057267c93258119e8f928bfd78
Milos Zivanovic <miloszivanovic@hotmail.com>
2025-07-05 17:14:20 +0200

    add sudo

--- install.sh
+++ install.sh
@@ -10,28 +10,28 @@ echo "Installing USB auto-mount..."
 
 # Copy scripts to /usr/local/bin
 echo "Copying usb-mount.sh to /usr/local/bin/"
-install -m 755 usb-mount.sh /usr/local/bin/usb-mount.sh
+sudo install -m 755 usb-mount.sh /usr/local/bin/usb-mount.sh
 
 echo "Copying usb-umount.sh to /usr/local/bin/"
-install -m 755 usb-umount.sh /usr/local/bin/usb-umount.sh
+sudo install -m 755 usb-umount.sh /usr/local/bin/usb-umount.sh
 
 # Copy systemd service files
 echo "Copying usb-mount@.service to /etc/systemd/system/"
-install -m 644 usb-mount@.service /etc/systemd/system/usb-mount@.service
+sudo install -m 644 usb-mount@.service /etc/systemd/system/usb-mount@.service
 
 echo "Copying usb-umount@.service to /etc/systemd/system/"
-install -m 644 usb-umount@.service /etc/systemd/system/usb-umount@.service
+sudo install -m 644 usb-umount@.service /etc/systemd/system/usb-umount@.service
 
 # Copy udev rules
 echo "Copying 99-usb-mount.rules to /etc/udev/rules.d/99-usb-mount.rules"
-install -m 644 99-usb-mount.rules /etc/udev/rules.d/99-usb-mount.rules
+sudo install -m 644 99-usb-mount.rules /etc/udev/rules.d/99-usb-mount.rules
 
 # Ensure shared group exists ---
 SHARED_GROUP="${SHARED_GROUP:-sharedmedia}"
 
 if ! getent group "$SHARED_GROUP" > /dev/null; then
     echo -t usb "Group $SHARED_GROUP not found, creating..."
-    groupadd --system "$SHARED_GROUP"
+    sudo groupadd --system "$SHARED_GROUP"
     echo -t usb "Group $SHARED_GROUP created"
 fi
 
@@ -47,8 +47,8 @@ echo -e "\033[0;33mAdded user 'www-data-nextcloud' to group '$SHARED_GROUP'.\033
 
 # Reload daemons
 echo "Reloading systemd and udev rules..."
-systemctl daemon-reexec
-udevadm control --reload-rules
-udevadm trigger
+sudo systemctl daemon-reexec
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 echo -e "\033[0;32mUSB auto-mount installed!\033[0m"
